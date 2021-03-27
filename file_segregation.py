cf0e1deb5f479a29bd41a305ff383ea3413bf58b
dunedin92 <m_lachowski@interia.eu>
2021-03-27 19:26:20 +0100

    wersja 2.0 (działająca)

--- file_segregation.py
+++ file_segregation.py
@@ -21,14 +21,12 @@ def file_segregation(source, destination, bom_path,  kolumna_part_number, kolumn
 
         part_number = sheet.cell(row=i, column=kolumna_part_number).value
         part_number = part_number.lstrip()
-        print(part_number)
         rysunek = sheet.cell(row=i, column=kolumna_rysunek).value
         tch1 = sheet.cell(row=i, column=kolumna_tch1).value
         tch2 = sheet.cell(row=i, column=kolumna_tch2).value
         tch3 = sheet.cell(row=i, column=kolumna_tch3).value
 
         if "WYKONANY" in rysunek.upper() or ("RYSUNEK" in rysunek.upper() and "SPAWALNICZY" in rysunek.upper()):
-            print('Istnieje rysunek do tej części')
             if tch1 != "-":
                 if tch2 != "-":
                     if tch3 != "-":
@@ -39,39 +37,46 @@ def file_segregation(source, destination, bom_path,  kolumna_part_number, kolumn
                     folder_name = tch1
 
                 folder_destiation = os.path.join(destination, folder_name)
-                print(folder_destiation)
 
-                if os.path.exists(folder_destiation):
-                    print('folder został juz utworzony')
-                else:
+                if not os.path.exists(folder_destiation):
                     os.mkdir(folder_destiation)
             else:
-                print(part_number)
-                print("dla tego pliku nie ma przypisanej obróbki")
-                no_file_in_surce.append(part_number + "brak podanej obróbki w BOM")
+                no_file_in_surce.append(part_number + " - brak podanej obróbki w BOM")
 
-            for format in formats:
-                part = part_number + format
-                print("-" * 60)
-                print(part)
-                part_destination = os.path.join(folder_destiation, part)
+            for path, dirs, files in os.walk(source):
+                for format in formats:
+                    part = part_number + format
+                    part_destination = os.path.join(folder_destiation, part)
 
-                for path, dirs, files in os.walk(source):
-                    print(path)
-                    part_source = os.path.join(path, part)
-                    if os.path.exists(part_source):
-                        if os.path.exists(part_destination):
-                            print("taki plik został już wczesniej przeniesiony")
-                            break
-                        else:
-                            shutil.copy(part_source, part_destination)
-                            break
+                    if os.path.exists(part_destination):
+                        break
                     else:
-                        if part in no_file_in_surce:
-                            print("brak tego pliku został już odnotowany")
-                        else:
-                            print('doddajemy rekord do tablicy')
-                            no_file_in_surce.append(part + " - " + folder_name)
+                        part_source = os.path.join(path, part)
+                        if os.path.exists(part_source):
+                            shutil.copy(part_source, part_destination)
+                            for i in no_file_in_surce:
+                                if part in i:
+                                    no_file_in_surce.remove(i)
 
-                    print(no_file_in_surce)
+                        else:
+                            flag = False
+                            if len(no_file_in_surce) < 1:
+                                no_file_in_surce.append(part + " - " + folder_name)
+
+                            else:
+                                for i in no_file_in_surce:
+                                    if part in i:
+                                        flag = False
+                                        break
+                                    else:
+                                        flag = True
+
+                                if flag == True:
+                                    no_file_in_surce.append(part + " - " + folder_name)
+
+
+#
+
+#
+            #print(no_file_in_surce)
     return no_file_in_surce
--- txt_file_creation.py
+++ txt_file_creation.py
@@ -1,13 +1,17 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# -*- coding: utf-8 -*-
+
 import os
 
 
 def txt_file_creation(destination, file_list, txt_file_name):
     txt_file_path = os.path.join(destination, txt_file_name)
-    plik = open(txt_file_path, "w")
+    plik = open(txt_file_path, "w", encoding='utf8')
 
     for name in file_list:
         plik.write(name)
-        plik. write("\n")
+        plik.write("\n")
     plik.close()
 
     return 0
