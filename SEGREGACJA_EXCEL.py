3eee6f1f976c6ada6ab112ef53d250abbc3b7c9d
dunedin92 <m_lachowski@interia.eu>
2021-03-28 12:35:57 +0200

    wersja 2.1 (działająca)

--- SEGREGACJA_EXCEL.py
+++ SEGREGACJA_EXCEL.py
@@ -8,15 +8,20 @@ from file_segregation import file_segregation
 from txt_file_creation import txt_file_creation
 
 
-# sciezka z której kopiujemy pliki:
-source = 'D:\PROGRAMOWANIE\M006'
-
-# scieżka do BOM'u i miejsce gdzie będą wrzucone posegregowane pliki:
-# destination = "D:\PROGRAMOWANIE\pliki"
+# pobranie linku do folderu skąd będą kopiowane wszystkie pliki:
+print("\n"")
+print("=" * 60)
+print("\n")
+source = input("Podaj scieżkę do miejsca, z którego będą kopiowane pliki.\n"
+               "Zostaną przeszukane wszystie foldery i podfoldery tej lokalizacji.\n ====>")
+print("=" * 60)
 
 # pobranie linku do foledru gdzie jest bom i beda kopiowane pliki:
 destination = input("Podaj scieżkę do pliku BOM, tutaj zostaną skopiowane posegregowane pliki.\n"
-                    " Upewnij się, że w folderze jest plik .xlsx i jest to BOM.\n ==>")
+                    " Upewnij się, że w folderze jest plik .xlsx i jest to BOM.\n ====>")
+print("=" * 60)
+
+# nazwa pliku tekstowego z brakujacymi plikami
 txt_file_name = "Lista_brakujacych_plikow.txt"
 
 if finding_bom(destination):
@@ -31,21 +36,27 @@ else:
 
 #jeżeli został znaleziony plik z BOMem to otwieramy go i pobieramy dane o interesujacych nas kolumnach:
 try:
+    print("Pobieranie danych o wierszach i kolumnach z pliku excel.")
     part_number, tch1, tch2, tch3, rysunek, max_row = excel(bom_path)
+    print("-" * 60)
 except:
-    print("Nie udało się pobrać danych z pliku excel, sprawdź czy zgadza się z szablonem")
+    print("Nie udało się pobrać danych z pliku excel, sprawdź czy zgadza się z szablonem.")
 
 #jeżeli pobrano dane z bomu to tworzymy foldery i wrzucamy do nich pliki:
 try:
+    print("Segregacja plików zgodnie z obróbkami w pliku excel.")
     no_file_in_source = file_segregation(source,destination, bom_path, part_number, tch1, tch2, tch3, rysunek, max_row)
+    print("-" * 60)
 except:
-   print("wystapił problem w trakcie segregacji")
+   print("Wystapił problem w trakcie segregacji.")
 
 #jezeli udało się posegregować pliki to wynikiem jest tabela z brakującymi elementami, poniższa funkcje wrzuca te dnae do pliku tekstowego
 try:
+    print("Zapis brakujących plików do pliku tekstowego: '" + txt_file_name + "'.")
     txt_file_creation(destination, no_file_in_source, txt_file_name)
+    print("-" * 60)
 except:
-   print("Nie udało się zapisać danych o brakujących elementach do pliku")
+   print("Nie udało się zapisać danych o brakujących elementach do pliku.")
 
 
 
